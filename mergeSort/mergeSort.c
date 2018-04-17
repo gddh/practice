cdc48ac4c2fbd17dcef57ebe4f9675dbfc839f4b
Derrick Lin <delin@e1z2r8p32.42.us.org>
2018-04-16 19:56:51 -0700

    completed mergeSort.c

--- mergeSort/mergeSort.c
+++ mergeSort/mergeSort.c
@@ -1,31 +1,88 @@
 #include <stdio.h>
 
-int     *mergeSort(int *arr, int len)
+/* Function to print an array */
+void printArray(int A[], int size)
 {
-    if (len <= 1)
-        return arr;
-    else
-        return merge(mergeSort(arr, len / 2), mergeSort(arr + len / 2, len / 2));
+    int i;
+    for (i=0; i < size; i++)
+        printf("%d ", A[i]);
+    printf("\n");
 }
 
 void    merge(int *arr, int l, int m, int r)
 {
-    int tmp1[m  - l];
-    int tmp2[r - m + 1];
+	int size1 = m - l + 1;
+	int size2 = r - m;
+    int tmp1[size1];
+    int tmp2[size2];
     int i;
-    int start = l;
+	int i1;
+	int i2;
 
     i = 0;
-    while (l < m)
-        tmp1[i++] = arr1[l++];
+    while (i < size1)
+	{
+        tmp1[i] = arr[l + i];
+		i = i + 1;
+	}
     i = 0;
-    while (m < r)
-        tmp2[i++] = arr2[m++];
+    while (i < size2)
+	{
+        tmp2[i] = arr[m + i + 1];
+		i = i + 1;
+	}
+	i = 0;
+	i1 = 0;
+	i2 = 0;
+	while (i1 < size1 && i2 < size2)
+	{
+		if (tmp1[i1] < tmp2[i2])
+			arr[l + i] = tmp1[i1++];
+		else
+			arr[l + i] = tmp2[i2++];
+		i = i + 1;
+	}
+	while (i1 < size1)
+	{	
+		arr[l + i] = tmp1[i1++];
+		i = i + 1;
+	}
+	while (i2 < size2)
+	{	
+		arr[l + i] = tmp2[i2++];
+		i = i + 1;
+	}
+}
 
-    while(start <= r)
-    {
-        
-        start = start + 1;
-    }
 
+
+int     *mergeSort(int *arr, int l, int r)
+{
+    if (r > l)
+	{
+		mergeSort(arr, l, (l + r) / 2);
+		mergeSort(arr, (l + r) / 2 + 1, r);
+        merge(arr, l, (l + r) / 2, r);	
+	}
+	return arr;
+}
+
+ 
+/* Driver program to test above functions */
+int main()
+{
+    //int arr[] = {12, 11, 13, 5, 6, 7};
+	//int arr[] = {1,2,5,2,4,3,7,6,9,8};
+	//int arr[] = {1};
+	int arr[0];
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+ 
+    printf("Given array is \n");
+    printArray(arr, arr_size);
+ 
+    mergeSort(arr, 0, arr_size - 1);
+ 
+    printf("\nSorted array is \n");
+    printArray(arr, arr_size);
+    return 0;
 }
